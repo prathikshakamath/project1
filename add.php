dbf20a0ee1922f76fc10426f4c1e887c74c970c9
pratibha jamkhandi <pratibha2191>
2021-06-16 21:39:47 +0530

    pull error corrected

--- add.php
+++ add.php
@@ -1,9 +1,6 @@
 <?php
-<<<<<<< HEAD
-include('timetable_add.php');
-=======
+
 include('session_admin.php');
->>>>>>> d0c95f71e93e9f8a26fba9ddc6c89d553babc4b0
 $ename = $_SESSION["ename"];
 $esem = $_SESSION["esem"];
 $eyear = $_SESSION["eyear"];
@@ -31,14 +28,6 @@ $id = $row['id'];
 $stime = $_POST["time"];
 $date = $_POST["date"];
 
-<<<<<<< HEAD
-//echo "{$array[1][2]}";
-for ($i = 0; $i < $_SESSION["count1"]; $i++) {
-    $cid = $array[$i][0];
-
-    $timestamp = strtotime($stime[$i]);
-    $etime = $timestamp + 3 * 360000;
-=======
 $time = "03:00:00";
 $secs = strtotime($time) - strtotime("00:00:00");
 //echo "{$array[1][2]}";
@@ -47,7 +36,7 @@ for ($i = 0; $i < $_SESSION["count1"]; $i++) {
     // echo $cid;
     $etime = "00:00:00";
 
->>>>>>> d0c95f71e93e9f8a26fba9ddc6c89d553babc4b0
+
     $sql = "INSERT INTO timetable(exam_id,course_id,admin_id,start_time,end_time,exam_date) VALUES ('$id','$cid','$loggedin_id','$stime[$i]','$etime','$date[$i]')";
     $result = mysqli_query($conn, $sql);
 }
