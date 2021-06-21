6311e1c5dcf551475243fec2fa80155a439df203
prathikshakamath <prathikshakamath.cs19@bmsce.ac.in>
2021-06-21 12:45:16 +0530

    no exam found added

--- exam.php
+++ exam.php
@@ -30,7 +30,7 @@
 
         .right hr {
             border: 1px solid #f1f1f1;
-            margin-bottom: 25px ;
+            margin-bottom: 25px;
         }
 
         .registerbtn {
@@ -47,40 +47,12 @@
         .registerbtn:hover {
             opacity: 2;
         }
-
     </style>
 
 </head>
 
 <body>
-    <?php
-    $tbl_name1 = "course";
-    $tbl_name2 = "course_enrolled";
-    $tbl_name3 = "exam";
-    $conn = mysqli_connect("$host", "$username", "$password") or die("cannot connect");
-    mysqli_select_db($conn, "$db_name") or die("cannot select DB");
 
-    date_default_timezone_set("Asia/Calcutta");
-    $cur_date = date("Y-m-d");
-
-    $sql = "SELECT e.exam_id AS eid,e.name as ename, e.sem as esem, e.start_date as edate FROM $tbl_name3 e
-            WHERE e.start_date > $cur_date AND e.sem IN (SELECT c.sem from $tbl_name2 ce,$tbl_name1 c,$tbl_name s 
-            WHERE s.email='$usermail'and ce.sem=s.current_sem and s.usn=ce.usn and ce.course_id=c.course_id)";
-
-    $res = mysqli_query($conn, $sql);
-    $_SESSION["count1"] = mysqli_num_rows($res);
-    $j = 0;
-    while ($row = mysqli_fetch_assoc($res)) {
-        $i = 0;
-        $array[$j][$i++] = $row['eid'];
-        $array[$j][$i++] = $row['ename'];
-        $array[$j][$i++] = $row['esem'];
-        $time = strtotime($row['edate']);
-        $array[$j][$i++] = date("F", $time);
-        $array[$j][$i++] = date("Y", $time);
-        $j++;
-    }
-    ?>
 
     <section class="right">
         <form method="POST" action="form.php" name="form2">
@@ -89,21 +61,56 @@
                 <h1> Exam Registeration Form</h1>
             </center>
             <hr>
-            <label class="field left">
-                SELECT EXAM:
-            </label>
-            <br>
             <?php
-            for ($i = 0; $i < $_SESSION["count1"]; $i++) {  ?>
-                <input type="radio" name="eid" value="<?php echo htmlspecialchars($array[$i][0]); ?>"><?php echo "<span> SEM:{$array[$i][2]} {$array[$i][1]} {$array[$i][3]}-{$array[$i][4]}</span> "; ?>
+            $tbl_name1 = "course";
+            $tbl_name2 = "course_enrolled";
+            $tbl_name3 = "exam";
+            $conn = mysqli_connect("$host", "$username", "$password") or die("cannot connect");
+            mysqli_select_db($conn, "$db_name") or die("cannot select DB");
+
+            date_default_timezone_set("Asia/Calcutta");
+            $cur_date = date("Y-m-d");
+
+            $sql = "SELECT e.exam_id AS eid,e.name as ename, e.sem as esem, e.start_date as edate FROM $tbl_name3 e
+            WHERE e.start_date > $cur_date AND e.sem IN (SELECT c.sem from $tbl_name2 ce,$tbl_name1 c,$tbl_name s 
+            WHERE s.email='$usermail'and ce.sem=s.current_sem and s.usn=ce.usn and ce.course_id=c.course_id)";
+
+            $res = mysqli_query($conn, $sql);
+            $_SESSION["count1"] = mysqli_num_rows($res);
+            if ($_SESSION["count1"] == 0) {
+                echo "NO EXAM FOUND<br>";
+                echo '<a href="student_login.php">Back to homepage</a>';
+            } else {
+                $j = 0;
+                while ($row = mysqli_fetch_assoc($res)) {
+                    $i = 0;
+                    $array[$j][$i++] = $row['eid'];
+                    $array[$j][$i++] = $row['ename'];
+                    $array[$j][$i++] = $row['esem'];
+                    $time = strtotime($row['edate']);
+                    $array[$j][$i++] = date("F", $time);
+                    $array[$j][$i++] = date("Y", $time);
+                    $j++;
+                }
+            }
+            if ($_SESSION["count1"] != 0) {
+            ?>
+                <label class="field left">
+                    SELECT EXAM:
+                </label>
                 <br>
+                <?php
+                for ($i = 0; $i < $_SESSION["count1"]; $i++) {  ?>
+                    <input type="radio" name="eid" value="<?php echo htmlspecialchars($array[$i][0]); ?>"><?php echo "<span> SEM:{$array[$i][2]} {$array[$i][1]} {$array[$i][3]}-{$array[$i][4]}</span> "; ?>
+                    <br>
 
-            <?php } ?>
+                <?php } ?>
 
-            <input type="submit" class="registerbtn" value="NEXT">
+                <input type="submit" class="registerbtn" value="NEXT">
 
         </form>
     </section>
+<?php } ?>
 </body>
 
 </html>
