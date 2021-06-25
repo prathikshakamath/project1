f0cf350cd1e15e0411e62e3b2dfffaa641308eb2
prathikshakamath <prathikshakamath.cs19@bmsce.ac.in>
2021-06-25 23:44:44 +0530

    home page corrected

--- home.php
+++ home.php
@@ -1,5 +1,6 @@
 ﻿<!doctype html>
 <html lang="en">
+
 <head>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -19,7 +20,7 @@
 
     <title>Home</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/carousel/"> 
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/carousel/">
 
     <!-- Bootstrap core CSS -->
     <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
@@ -49,16 +50,16 @@
             background-color: #3aafa9;
             text-align: center;
             padding-top: 10%;
-            background-image: linear-gradient(to right top,rgba(136,96,208,0.3),rgba(193,200,228,0.5),rgba(86,128,233,0.6),rgba(90,185,234,0.4),rgba(193,200,228,0.5),rgba(132,206,235,0.6),rgba(193,200,228,0.5),rgba(136,96,208,0.4)),url(images/img2.jpg);
+            background-image: linear-gradient(to right top, rgba(136, 96, 208, 0.3), rgba(193, 200, 228, 0.5), rgba(86, 128, 233, 0.6), rgba(90, 185, 234, 0.4), rgba(193, 200, 228, 0.5), rgba(132, 206, 235, 0.6), rgba(193, 200, 228, 0.5), rgba(136, 96, 208, 0.4)), url(images/img2.jpg);
             background-repeat: no-repeat;
             background-attachment: fixed;
             background-size: 100% 100%;
         }
 
-            .full img {
-                width: 40%;
-                margin-top: 3%;
-            }
+        .full img {
+            width: 40%;
+            margin-top: 3%;
+        }
 
         .ml14 {
             font-weight: 400;
@@ -70,29 +71,29 @@
             text-shadow: 3px 3px rgba(26, 41, 247, 1);
         }
 
-            .ml14 .text-wrapper {
-                position: relative;
-                display: inline-block;
-                padding-top: 0.1em;
-                padding-right: 0.05em;
-                padding-bottom: 0.15em;
-            }
+        .ml14 .text-wrapper {
+            position: relative;
+            display: inline-block;
+            padding-top: 0.1em;
+            padding-right: 0.05em;
+            padding-bottom: 0.15em;
+        }
 
-            .ml14 .line {
-                opacity: 0;
-                position: absolute;
-                left: 0;
-                height: 2px;
-                width: 100%;
-                background-color: #fff;
-                transform-origin: 100% 100%;
-                bottom: 0;
-            }
+        .ml14 .line {
+            opacity: 0;
+            position: absolute;
+            left: 0;
+            height: 2px;
+            width: 100%;
+            background-color: #fff;
+            transform-origin: 100% 100%;
+            bottom: 0;
+        }
 
-            .ml14 .letter {
-                display: inline-block;
-                line-height: 1em;
-            }
+        .ml14 .letter {
+            display: inline-block;
+            line-height: 1em;
+        }
 
         .full img {
             width: 40%;
@@ -124,7 +125,6 @@
         .left {
             width: 59%;
             height: 650px;
-            display: inline-block;
             float: left;
         }
 
@@ -143,16 +143,39 @@
             opacity: 1 !important;
         }
 
-            .navbar ul li a:hover {
-                color: #c1c8e4 !important;
-            }
+        .navbar ul li a:hover {
+            color: #c1c8e4 !important;
+        }
 
         .para {
             margin-top: 20%;
         }
+
+        footer {
+            background-color: black;
+            display: inline-block;
+            width: 100%;
+            padding: 2%;
+        }
+
+        .div {
+            display: inline-block;
+            width: 33%;
+            color: #c1c8e4;
+            margin: auto;
+            padding: auto;
+            text-align: center;
+            align-content: space-around;
+        }
+
+        div .img {
+            display: inline-block;
+            width: 50%;
+        }
     </style>
 
 </head>
+
 <body>
 
     <header>
@@ -164,10 +187,10 @@
             <div class="collapse navbar-collapse" id="navbarCollapse">
                 <ul class="navbar-nav mr-auto" style="font-size:120%;">
                     <li class="nav-item">
-                        <a class="nav-link" href="#" >Home </a>
+                        <a class="nav-link" href="#">Home </a>
                     </li>
-                    <li class="nav-item" >
-                        <a class="nav-link" href="#about" >About</a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#contact">Contact</a>
@@ -187,7 +210,9 @@
             var textWrapper = document.querySelector('.ml3');
             textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-            anime.timeline({ loop: false })
+            anime.timeline({
+                    loop: false
+                })
                 .add({
 
                     targets: '.ml3 .letter',
@@ -206,8 +231,10 @@
         <script>
             var textWrapper = document.querySelector('.ml14 .letters');
             textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-   
-            anime.timeline({ loop: false})
+
+            anime.timeline({
+                    loop: false
+                })
                 .add({
                     targets: '.ml14 .line',
                     scaleX: [0, 1],
@@ -222,7 +249,9 @@
                     delay: 2000
                 });
 
-            anime.timeline({ loop: true })
+            anime.timeline({
+                    loop: true
+                })
                 .add({
                     delay: 2000,
                     targets: '.ml14 .letter',
@@ -240,80 +269,49 @@
                     duration: 1000,
                     easing: "easeOutExpo",
                     delay: 1000
-                });               
+                });
         </script>
         <img src="images/bms_logo.png" />
-   
+
     </div>
-            
-    
-                <div class="left" >
-                    <video autoplay muted loop id="myVideo">
-                        <source src="images/bms_video.mov" type="video/mp4">
-                    </video>
-                </div>
-         
-                <div class="right" id="about">
-                    <div class="para">
-                    <p class="lead">B.M.S. College of Engineering (BMSCE) is an autonomous engineering college in Basavangudi, Bangalore, India. It was started in 1946 by Bhusanayana Mukundadas Sreenivasaiah and is run by the B.M.S. Educational Trust.It is affiliated with Visvesvaraya Technological University and became autonomous in 2008. BMSCE is located on Bull Temple Road, Basavanagudi, diagonally opposite to the famous Bull Temple. Though a private engineering college, it is partially funded by the Government of Karnataka.</p>
-                    <p class="lead" style="font-family: 'Roboto Condensed'; font-size:160%; ">
-                        Rankings
-                        <ul style="font-family: 'Roboto Condensed'; font-size:130%; "><li>
-                        NIRF (2019)	69</li>
-                        <li>Outlook India (2019)  40</li>
-                       <li>India Today (2020)	24</li></ul>
-         
-                    </p>
-                        </div>
-                </div>
-
-       
-
-  
-
-                <div class="container-fluid pb-0 mb-0 justify-content-center text-light " style="background-color:black;height:550px;" id="contact">
-                    <hr class="featurette-divider">
-
-                    <h1 class="" style="">Contact</h1>
-                    <div class="row test">
-                        <div class="col-md-4 text-center" style="padding-left:2%;">
-                            <img src="images/bmsce_logo.png" style="width:60%;">
-                            <p style="padding-top:1%; font-size:120%;"><i class="fas fa-map-marker-alt"></i><span class="glyphicon glyphicon-map-marker"></span> Bangalore,India</p>
-                            <div>
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d497699.9974198194!2d77.35072903896105!3d12.953847709359573!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1670c9b44e6d%3A0xf8dfc3e8517e4fe0!2sBengaluru%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1596180539411!5m2!1sen!2sin" width="150" height="60" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-                            </div>
-                            <p style="padding-top:1%; font-size:120%;"><i class="fas fa-phone-square"></i><span class="glyphicon glyphicon-phone"></span> Phone: +91 1111111111</p>
-                            <p style="padding-top:1%; font-size:120%;"><i class="fas fa-envelope"></i><span class="glyphicon glyphicon-envelope"></span> Email: bmsce@bmsce.ac.in</p>
-                        </div>
-                        <div class="col-md-8" style="margin-right:2%; margin-left:-3%;">
-
-                            <div class="row" style="margin-top:5%;">
-
-                                <div class="col-sm-6 form-group">
-                                    <input class="form-control" id="name" name="name" placeholder="Name" type="text" [A-Za-z ]{1,} required style="width:100%;height:120%;">
-                                </div><br>
-                                <div class="col-sm-6 form-group">
-                                    <input class="form-control" id="email" name="email" placeholder="Email" type="email"  pattern="[a-z0-9.]+@+[a-z.]$" required style="width:100%;height:120%;">
-                                </div>
-                            </div>
-                            <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5" style="width:100%;height:50%;margin-top:4%;"></textarea>
-                            <div class="row">
-                                <div class="col-md-12 form-group">
-                                    <button class="btn pull-right btn-outline-success" type="submit" style="margin-left:92%; margin-top:2%; width:8%; color:white;" onclick="reset()">Send</button>
-                                    <script>
-                                        function reset() {
-                                            document.getElementById('name').value = "";
-                                            document.getElementById('email').value = "";
-                                            document.getElementById('comments').value = "";
-                                        }
-                                    </script>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-        </main>
 
 
+    <div class="left">
+        <video autoplay muted loop id="myVideo">
+            <source src="images/bms_video.mov" type="video/mp4">
+        </video>
+    </div>
+
+    <div class="right" id="about">
+        <div class="para">
+            <p class="lead">B.M.S. College of Engineering (BMSCE) is an autonomous engineering college in Basavangudi, Bangalore, India. It was started in 1946 by Bhusanayana Mukundadas Sreenivasaiah and is run by the B.M.S. Educational Trust.It is affiliated with Visvesvaraya Technological University and became autonomous in 2008. BMSCE is located on Bull Temple Road, Basavanagudi, diagonally opposite to the famous Bull Temple. Though a private engineering college, it is partially funded by the Government of Karnataka.</p>
+            <p class="lead" style="font-family: 'Roboto Condensed'; font-size:160%; ">
+                Rankings
+            <ul style="font-family: 'Roboto Condensed'; font-size:130%; ">
+                <li>
+                    NIRF (2019) 69</li>
+                <li>Outlook India (2019) 40</li>
+                <li>India Today (2020) 24</li>
+            </ul>
+
+            </p>
+        </div>
+    </div>
+
+    <footer>
+        <div class="div">
+            <img class="img" src="images/bmsce_logo.png">
+        </div>
+        <div class="div second">
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d497699.9974198194!2d77.35072903896105!3d12.953847709359573!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1670c9b44e6d%3A0xf8dfc3e8517e4fe0!2sBengaluru%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1596180539411!5m2!1sen!2sin" width="180" height="100" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            <p><i class="fas fa-map-marker-alt"></i><span class="glyphicon glyphicon-map-marker"></span> Bangalore,India</p>
+        </div>
+        <div class="div third">
+            <p><i class="fas fa-phone-square"></i><span class="glyphicon glyphicon-phone"></span> Phone: +91 1111111111</p>
+            <p><i class="fas fa-envelope"></i><span class="glyphicon glyphicon-envelope"></span> Email: bmsce@bmsce.ac.in</p>
+        </div>
+    </footer>
 </body>
+
 </html>
