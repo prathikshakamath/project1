48c23d561d2cd303895e27b2086862a45c4a03b8
pratibha jamkhandi <pratibha2191>
2021-06-26 18:10:07 +0530

    css changed

--- password.php
+++ password.php
@@ -1,157 +1,179 @@
 <html>
+
 <head>
     
     <?php
     include('session.php');
     //session_start();
     ?>
-<title>Change Password</title>
+    <title>Change Password</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/f293a21338.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="styles.css" />
 
-<link rel="stylesheet" type="text/css" href="styles.css" />
-<style>
-body {
-font-family:Arial;
-}
-input {
-font-family:Arial;
-font-size:14px;
-}
-label{
-font-family:Arial;
-font-size:14px;
-color:#999999;
-}
-.tblSaveForm {
-border-top:2px #999999 solid;
-background-color: #f8f8f8;
-}
-.tableheader {
-background-color: #fedc4d;
-}
-.btnSubmit {
-background-color:#fd9512;
-padding:5px;
-border-color:#FF6600;
-border-radius:4px;
-color:white;
-}
-.message {
-color: #FF0000;
-text-align: center;
-width: 100%;
-}
-.txtField {
-padding: 5px;
-border:#fedc4d 1px solid;
-border-radius:4px;
-}
-.required {
-color: #FF0000;
-font-size:11px;
-font-weight:italic;
-padding-left:10px;
-}
-.main-content {
-margin: 0 auto;
-max-width: 820px;
-}
-.main-content__checkmark {
-font-size: 4.0625rem;
-line-height: 1;
-color: #24b663;
-}
-.main-content__body {
-margin: 20px 0 0;
-font-size: 1rem;
-line-height: 1.4;
-}
-.main-content__checkmark {
-font-size: 9.75rem;
-}           
-.main-content__body {               
-font-size: 1.25rem;
-}
-        
-</style>
-<script>
-function validatePassword() {
-var currentPassword,newPassword,confirmPassword,output = true;
+    <style>
+        body {
+            font-family: Arial;
+        }
 
-currentPassword = document.frmChange.currentPassword;
-newPassword = document.frmChange.newPassword;
-confirmPassword = document.frmChange.confirmPassword;
+        input {
+            font-family: Arial;
+            font-size: 14px;
+        }
 
-if(!currentPassword.value) {
-currentPassword.focus();
-document.getElementById("currentPassword").innerHTML = "required";
-output = false;
-}
-else if(!newPassword.value) {
-newPassword.focus();
-document.getElementById("newPassword").innerHTML = "required";
-output = false;
-}
-else if(!confirmPassword.value) {
-confirmPassword.focus();
-document.getElementById("confirmPassword").innerHTML = "required";
-output = false;
-}
-if(newPassword.value != confirmPassword.value) {
-newPassword.value="";
-confirmPassword.value="";
-newPassword.focus();
-document.getElementById("confirmPassword").innerHTML = "not same";
-output = false;
-} 	
-return output;
-}
-</script>
+        label {
+            font-family: Arial;
+            font-size: 16px;
+            color: black;
+        }
+
+        .tblSaveForm {
+            background-color: aliceblue;
+        }
+
+        .tableheader {
+            background-color: #4682B4;
+            color: aliceblue;
+            text-align: center;
+            font-weight: 120%;
+            font-size: larger;
+        }
+
+        .btnSubmit {
+            padding: 5px;
+            border-color: #FF6600;
+            border-radius: 4px;
+            background-color: #4CAF50;
+            color: white;
+            padding: 2%;
+            margin-left: 75%;
+            border: none;
+            cursor: pointer;
+            width: 20%;
+            opacity: 0.9;
+        }
+
+        .btnSubmit:hover {
+            opacity: 2;
+            border: 1px solid black;
+        }
+
+        .full {
+            display: inline-block;
+            width: 50%;
+            margin-left: 30%;
+            margin-top: 13%;
+        }
+
+        .message {
+            color: #FF0000;
+            text-align: left;
+            width: 100%;
+        }
+
+        .txtField {
+            padding: 5px;
+            border: #fedc4d 1px solid;
+            border-radius: 4px;
+        }
+
+        .required {
+            color: #FF0000;
+            font-size: 11px;
+            font-weight: italic;
+            padding-left: 10px;
+        }
+
+        .link {
+            margin-left: 40%;
+            font-size: larger;
+        }
+
+        a:hover {
+            color: #EE4B2B;
+        }
+    </style>
+    <script>
+        function validatePassword() {
+            var currentPassword, newPassword, confirmPassword, output = true;
+
+            currentPassword = document.frmChange.currentPassword;
+            newPassword = document.frmChange.newPassword;
+            confirmPassword = document.frmChange.confirmPassword;
+
+            if (!currentPassword.value) {
+                currentPassword.focus();
+                document.getElementById("currentPassword").innerHTML = "REQUIRED";
+                output = false;
+            } else if (!newPassword.value) {
+                newPassword.focus();
+                document.getElementById("newPassword").innerHTML = "REQUIRED";
+                output = false;
+            } else if (!confirmPassword.value) {
+                confirmPassword.focus();
+                document.getElementById("confirmPassword").innerHTML = "REQUIRED";
+                output = false;
+            }
+            if (newPassword.value != confirmPassword.value) {
+                newPassword.value = "";
+                confirmPassword.value = "";
+                newPassword.focus();
+                document.getElementById("confirmPassword").innerHTML = "NOT SAME";
+                output = false;
+            }
+            return output;
+        }
+    </script>
 </head>
-<body>
-	
-<?php
-$_SESSION["usn"]="$loggedin_usn ";
-$conn = mysqli_connect("$host", "$username", "$password") or die("cannot connect");
-mysqli_select_db($conn, "$db_name") or die("cannot select DB");
 
-if (count($_POST) > 0) {
-    $result = mysqli_query($conn, "SELECT *from student WHERE usn='" . $_SESSION["usn"] . "'");
-    $row = mysqli_fetch_array($result);
-    if ($_POST["currentPassword"] == $row['password']) {
-        mysqli_query($conn, "UPDATE student set password='" . $_POST["newPassword"] . "' WHERE usn='" . $_SESSION["usn"] . "'");
-        $message = "Password Changed";
-    } else
-        $message = "Current Password is not correct";
-}
-?>
-<form name="frmChange" method="post" action="" onSubmit="return validatePassword()">
-<div style="width:500px;">
-<div class="message"><?php if(isset($message)) { echo $message; } ?></div>
-<table border="0" cellpadding="10" cellspacing="0" width="500" align="center" class="tblSaveForm">
-<tr class="tableheader">
-<td colspan="2">Change Password</td>
-</tr>
-<tr>
-<td width="40%"><label>Current Password</label></td>
-<td width="60%"><input type="password" name="currentPassword" class="txtField"/><span id="currentPassword"  class="required"></span></td>
-</tr>
-<tr>
-<td><label>New Password</label></td>
-<td><input type="password" name="newPassword" class="txtField"/><span id="newPassword" class="required"></span></td>
-</tr>
-<td><label>Confirm Password</label></td>
-<td><input type="password" name="confirmPassword" class="txtField"/><span id="confirmPassword" class="required"></span></td>
-</tr>
-<tr>
-<td colspan="2"><input type="submit" name="submit" value="Submit" class="btnSubmit"></td>
-</tr>
-</table>
-</div>
-</form>
-<div class="main-content">
-        <i class="fa fa-check main-content__checkmark" id="checkmark"></i>
-        <p class="main-content__body" data-lead-id="main-content-body"><a href="student_homepage.php" style="text-decoration:none;"><i class="fas fa-undo-alt"></i> Back to Homepage</a> </p>
+<body>
+
+    <?php
+    $_SESSION["usn"] = "$loggedin_usn ";
+    $conn = mysqli_connect("$host", "$username", "$password") or die("cannot connect");
+    mysqli_select_db($conn, "$db_name") or die("cannot select DB");
+
+    if (count($_POST) > 0) {
+        $result = mysqli_query($conn, "SELECT *from student WHERE usn='" . $_SESSION["usn"] . "'");
+        $row = mysqli_fetch_array($result);
+        if ($_POST["currentPassword"] == $row['password']) {
+            mysqli_query($conn, "UPDATE student set password='" . $_POST["newPassword"] . "' WHERE usn='" . $_SESSION["usn"] . "'");
+            $message = "Password Changed";
+        } else
+            $message = "Current Password is not correct";
+    }
+    ?>
+    <div class="full">
+        <form class="form" name="frmChange" method="post" action="" onSubmit="return validatePassword()">
+            <div style="width:500px;">
+                <div class="message"><?php if (isset($message)) {
+                                            echo "<span>$message<span>";
+                                        } ?></div>
+                <table border="0" cellpadding="10" cellspacing="0" width="500" align="center" class="tblSaveForm">
+                    <tr class="tableheader">
+                        <td colspan="2">Change Password</td>
+                    </tr>
+                    <tr>
+                        <td width="40%"><label>Current Password</label></td>
+                        <td width="60%"><input type="password" name="currentPassword" class="txtField" /><span id="currentPassword" class="required"></span></td>
+                    </tr>
+                    <tr>
+                        <td><label>New Password</label></td>
+                        <td><input type="password" name="newPassword" class="txtField" /><span id="newPassword" class="required"></span></td>
+                    </tr>
+                    <td><label>Confirm Password</label></td>
+                    <td><input type="password" name="confirmPassword" class="txtField" /><span id="confirmPassword" class="required"></span></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"><input type="submit" name="submit" value="Submit" class="btnSubmit"></td>
+                    </tr>
+                </table>
+            </div>
+        </form>
+
+        <a class="link" href="student_homepage.php" style="text-decoration:none;"><i class="fas fa-undo-alt"></i> Back to Homepage</a>
     </div>
 
 </body>
+
 </html>
--- profile_photo.php
+++ profile_photo.php
@@ -23,10 +23,10 @@ if (isset($_POST['but_upload'])) {
             // Insert record
             $url = $loggedin_usn . "." . $imageFileType;
             //echo "{$url}";
-
+            $ans = 0;
             $sql = "UPDATE student SET image_url='$url' WHERE usn='$loggedin_usn'";
             if (mysqli_query($conn, $sql)) {
-                echo "Profile updated successfully";
+                $ans = 1;
                 $_SESSION['profile_url'] = "upload/" . $url;
                 //echo $_SESSION['profile_url'];
             } else {
@@ -36,11 +36,73 @@ if (isset($_POST['but_upload'])) {
     }
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<form method="post" action="" enctype='multipart/form-data'>
-    <input type='file' name='file' />
-    <input type='submit' value='Save name' name='but_upload'>
-</form>
-<?php
-echo '<p class="link"><a href="student_homepage.php" style="text-decoration:none;"><i class="fas fa-undo-alt"></i> Back to homepage</a></p>';
-?>
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/f293a21338.js" crossorigin="anonymous"></script>
+    <title>Student Homepage</title>
+    <style>
+        body {
+            background-color: lightblue;
+        }
+
+        input[type=file] {
+            margin: 5px 2px 22px 5px;
+            display: inline-block;
+            height: 20%;
+            font-size: larger;
+        }
+
+        hr {
+            border: 1px solid #f1f1f1;
+            margin-bottom: 25px;
+        }
+
+        .registerbtn {
+            font-size: larger;
+            background-color: #4CAF50;
+            color: white;
+            padding: 10px 12px;
+            border: none;
+            cursor: pointer;
+            width: 15%;
+            opacity: 0.9;
+        }
+
+        .registerbtn:hover {
+            opacity: 2;
+            border: 1px solid black;
+        }
+
+        .link {
+            font-size: larger;
+        }
+
+        .link a:hover {
+            color: #EE4B2B;
+        }
+    </style>
+
+<body>
+    <form method="post" action="" enctype='multipart/form-data'>
+        <center>
+            <h1> Change Profile Picture</h1>
+        </center>
+        <hr>
+        <input type='file' name='file' />
+        <input type='submit' class='registerbtn' value='Change' name='but_upload'>
+    </form>
+    <?php
+    if (isset($ans)) {
+        if ($ans == 1) {
+            $ans == 0;
+            echo "<span>Profile updated successfully<span>";
+        }
+    }
+    ?>
+    <p class="link"><a href="student_homepage.php" style="text-decoration:none;"><i class="fas fa-undo-alt"></i> Back to homepage</a></p>
+</body>
+
+</html>
